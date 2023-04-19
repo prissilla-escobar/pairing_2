f18778fe7c22b6d6f6021979ce79235a6915358a
Prissilla Escobar <prissilla988@gmail.com>
2023-04-19 17:50:44 -0600

    Add places to visit

--- friends.js
+++ friends.js
@@ -9,3 +9,9 @@ console.log(arrays[3])
 console.log(arrays[0])
 
 console.log(arrays.slice(1,3))
+
+var places = ["Hawaii", "Norway", "Thailand", "Japan", "Costa Rica"]
+
+console.log(places.slice(2,4))
+
+
